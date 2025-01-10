9c2d6b4261ff96abbef9005003a1a59819a78b8a
Hukissy Kulembe <ukissyulembe7@gmail.com>
2025-01-11 00:14:33 +0100

    feat: Primeiro commit, contendo no arquivo properties os dados configurado para a coneção com o banco de dados postgreSQL.

---
+++ Free-Library/src/main/java/com/ao/kulembe/Free/Library/FreeLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ao.kulembe.Free.Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeLibraryApplication.class, args);
+	}
+
+}
---
+++ Free-Library/src/test/java/com/ao/kulembe/Free/Library/FreeLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ao.kulembe.Free.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
