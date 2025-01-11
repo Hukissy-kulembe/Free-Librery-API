156f3f5f2d3a12e70737ec0dc83ccd4231ec0042
Hukissy Kulembe <ukissyulembe7@gmail.com>
2025-01-11 19:21:31 +0100

    feat: classes auxiliares.

--- Free-Library/src/main/java/com/ao/kulembe/Free/Library/services/uteis/Uteis.java
+++ Free-Library/src/main/java/com/ao/kulembe/Free/Library/services/uteis/Uteis.java
@@ -1,4 +1,26 @@
 package com.ao.kulembe.Free.Library.services.uteis;
 
+import com.ao.kulembe.Free.Library.models.Autor;
+import com.ao.kulembe.Free.Library.models.Genero;
+import com.ao.kulembe.Free.Library.models.Usuario;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Uteis {
+
+    public static Set<String> autores(Set<Autor> autores) {
+        return autores.stream()
+                .map(x -> x.getNome()).collect(Collectors.toSet());
+    }
+
+    public static Set<String> generos(Set<Genero> generos) {
+        return generos.stream()
+                .map(x -> x.getNome()).collect(Collectors.toSet());
+    }
+
+    public static Set<String> usuarios(Set<Usuario> usuarios) {
+        return usuarios.stream()
+                .map(x -> x.getNome()).collect(Collectors.toSet());
+    }
 }
