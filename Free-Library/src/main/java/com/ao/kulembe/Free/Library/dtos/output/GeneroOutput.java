b6ed82df920af05266371b5708ddcd0014dd56bb
Hukissy Kulembe <ukissyulembe7@gmail.com>
2025-01-12 17:41:30 +0100

    refactor: remoção do parametro String[] livros e adição do id.

--- Free-Library/src/main/java/com/ao/kulembe/Free/Library/dtos/output/GeneroOutput.java
+++ Free-Library/src/main/java/com/ao/kulembe/Free/Library/dtos/output/GeneroOutput.java
@@ -1,8 +1,8 @@
 package com.ao.kulembe.Free.Library.dtos.output;
 
 public record GeneroOutput(
+        Long id,
         String nome,
-        String descricao,
-        String[] livros
+        String descricao
 ) {
 }
