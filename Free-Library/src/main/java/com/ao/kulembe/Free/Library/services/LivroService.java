ed00fea91d56ff7f022ee97b92f091efb62c3991
Hukissy Kulembe <ukissyulembe7@gmail.com>
2025-01-18 17:38:30 +0100

    refactor: refatorei de List para o Set.

--- Free-Library/src/main/java/com/ao/kulembe/Free/Library/services/LivroService.java
+++ Free-Library/src/main/java/com/ao/kulembe/Free/Library/services/LivroService.java
@@ -4,19 +4,20 @@ import com.ao.kulembe.Free.Library.dtos.input.LivroInput;
 import com.ao.kulembe.Free.Library.dtos.output.AutorOutput;
 import com.ao.kulembe.Free.Library.dtos.output.LivroOutput;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.Set;
 
 public interface LivroService {
 
-    LivroOutput cadastrar(LivroInput livroDtoInput);
+    LivroOutput cadastrar(LivroInput livroDtoInput) throws IOException;
     LivroOutput buscarPorId(Long id);
     void deletarPorId(Long id);
     LivroOutput buscarPorIsbn(String isbn);
     LivroOutput buscarPorTitulo(String titulo);
-    List<LivroOutput> buscarTodos(String titulo, String autor, String genero, String editora);
-    LivroOutput atualizar(Long id, LivroInput livroDtoInput);
-    List<AutorOutput> listarAutores(String livro);
-    List<LivroOutput> buscarLivrosPorAutor(String nome);
-    List<LivroOutput> buscarLivrosPorGenero(String genero);
+    Set<LivroOutput> buscarTodos(String titulo, String autor, String genero, String editora);
+    LivroOutput atualizar(Long id, LivroInput livroDtoInput) throws IOException;
+    Set<AutorOutput> listarAutores(String livro);
+    Set<LivroOutput> buscarLivrosPorAutor(String nome);
+    Set<LivroOutput> buscarLivrosPorGenero(String genero);
 
 }
