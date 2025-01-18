c3b1346b91f2f5d77428843082f2c1dfb89c7d44
Hukissy Kulembe <ukissyulembe7@gmail.com>
2025-01-18 17:35:29 +0100

    feat: implementação da lógica.

--- Free-Library/src/main/java/com/ao/kulembe/Free/Library/services/implementations/ArquivoImplemente.java
+++ Free-Library/src/main/java/com/ao/kulembe/Free/Library/services/implementations/ArquivoImplemente.java
@@ -1,4 +1,64 @@
 package com.ao.kulembe.Free.Library.services.implementations;
 
-public class ArquivoImplemente {
+import com.ao.kulembe.Free.Library.dtos.input.ArquivoInput;
+import com.ao.kulembe.Free.Library.dtos.output.ArquivoOutput;
+import com.ao.kulembe.Free.Library.models.Arquivo;
+import com.ao.kulembe.Free.Library.repositories.ArquivoRepository;
+import com.ao.kulembe.Free.Library.services.ArquivoService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class ArquivoImplemente implements ArquivoService {
+
+    private final ArquivoRepository arquivoRepository;
+
+    public ArquivoImplemente(ArquivoRepository arquivoRepository) {
+        this.arquivoRepository = arquivoRepository;
+    }
+
+    @Override
+    public ArquivoOutput salvarArquivo(ArquivoInput arquivoInput) {
+        try {
+            MultipartFile capa = arquivoInput.capa();
+            MultipartFile arquivo = arquivoInput.arquivo();
+
+            Arquivo novoArquivo = new Arquivo(
+                    capa.getBytes(),
+                    arquivo.getBytes()
+            );
+
+            Arquivo arquivoSalvo = arquivoRepository.save(novoArquivo);
+
+            return new ArquivoOutput(
+                    arquivoSalvo.getId(),
+                    arquivoSalvo.getCapa(),
+                    arquivoSalvo.getArquivo()
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao salvar o arquivo", e);
+        }
+    }
+
+    @Override
+    public ArquivoOutput busalvarArquivoId(Long id) {
+        Arquivo arquivo = arquivoRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Arquivo não encontrado para o ID: " + id));
+        return new ArquivoOutput(
+                arquivo.getId(),
+                arquivo.getCapa(),
+                arquivo.getArquivo()
+        );
+    }
+
+    @Override
+    public void deletarPorId(Long id) {
+        arquivoRepository.deleteById(id);
+    }
+
 }
