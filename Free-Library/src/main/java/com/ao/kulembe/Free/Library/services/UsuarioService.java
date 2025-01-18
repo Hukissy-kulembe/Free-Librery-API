3986f2c79d2f469cfd9a613935c09490a99b7c83
Hukissy Kulembe <ukissyulembe7@gmail.com>
2025-01-18 20:27:15 +0100

    refactor: remoção de List para Set.

--- Free-Library/src/main/java/com/ao/kulembe/Free/Library/services/UsuarioService.java
+++ Free-Library/src/main/java/com/ao/kulembe/Free/Library/services/UsuarioService.java
@@ -1,18 +1,19 @@
 package com.ao.kulembe.Free.Library.services;
 
 import com.ao.kulembe.Free.Library.dtos.input.UsuarioInput;
+import com.ao.kulembe.Free.Library.dtos.output.LivroOutput;
 import com.ao.kulembe.Free.Library.dtos.output.UsuarioOutput;
 
-import java.util.List;
+import java.util.Set;
 
 public interface UsuarioService {
 
     UsuarioOutput cadastraUsuario(UsuarioInput usuarioInput);
     UsuarioOutput buscarPorId(Long id);
     void deletarPorId(Long id);
-    List<UsuarioOutput> listarUsuarios();
+    Set<UsuarioOutput> listarUsuarios();
     UsuarioOutput buscarPorNome(String nome);
     UsuarioOutput atualizar(Long id, UsuarioInput usuarioInput);
-    List<UsuarioOutput> buscarLivrosDeUmUsuario(String usuario);
+    Set<LivroOutput> buscarLivrosDeUmUsuario(String usuario);
 
 }
