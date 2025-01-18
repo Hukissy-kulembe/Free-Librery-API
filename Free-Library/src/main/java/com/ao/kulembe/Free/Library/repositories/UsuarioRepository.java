51690da3a1b6ad13ac71b38ffcd9696649f75ccd
Hukissy Kulembe <ukissyulembe7@gmail.com>
2025-01-18 20:29:57 +0100

    feat: adicionei a anotação @Query e método findByName.

--- Free-Library/src/main/java/com/ao/kulembe/Free/Library/repositories/UsuarioRepository.java
+++ Free-Library/src/main/java/com/ao/kulembe/Free/Library/repositories/UsuarioRepository.java
@@ -2,6 +2,12 @@ package com.ao.kulembe.Free.Library.repositories;
 
 import com.ao.kulembe.Free.Library.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    @Query("SELECT u FROM Usuario WHERE u.nome = :nome")
+    Optional<Usuario> findByName(String name);
 }
